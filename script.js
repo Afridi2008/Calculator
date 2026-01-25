69bac94ca7fc6604e63f8efb7581d00c309642be
Afridi2008 <afridimohamed9741@gmail.com>
2026-01-25 12:16:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.getElementById("display");
+function appendvalue(value) {
+    display.value = display.value + value;
+}
+function cleardisplay() {
+    display.value = "";
+}
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch {
+        display.value = "Error";
+    }
+}
